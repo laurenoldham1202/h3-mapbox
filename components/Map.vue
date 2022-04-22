5b292ead88b6d2cd73d3381fd35e80e7c170d2f9
laurenoldham1202 <lauren.oldham@uky.edu>
2022-04-22 10:47:52 -0400

    Test hex from bbox polygons

--- components/Map.vue
+++ components/Map.vue
@@ -7,7 +7,7 @@
     import * as M from 'mapbox-gl'
     import geojson2h3 from 'geojson2h3';
     import * as h3 from "h3-js";
-    import GeoJSON from 'GeoJSON'
+    import { GeoJSON } from 'GeoJSON'
 
     export interface HexObject {
         [key: string]: number
@@ -22,115 +22,182 @@
 
     export default Vue.extend({
         data: () => ({
-            map: undefined as any
+            map: undefined as any,
+            coords: { lng: -76.486729, lat: 42.479490} as M.LngLat,
+            bbox: [[-104.6527823561,36.3116770845],[-94.7570348445,36.3116770845],[-94.7570348445,42.9637944979],[-104.6527823561,42.9637944979],[-104.6527823561,36.3116770845]] as any,
+            bboxRev: [[36.3116770845,-104.6527823561],[36.3116770845,-94.7570348445],[42.9637944979,-94.7570348445],[42.9637944979,-104.6527823561],[36.3116770845,-104.6527823561]] as any
         }),
         mounted(): void {
             (M as any).accessToken = 'pk.eyJ1IjoibGF1cmVub2xkaGFtMTIwMiIsImEiOiJjaW55dm52N2gxODJrdWtseWZ5czAyZmp5In0.YkEUt6GvIDujjudu187eyA'
             this.map = new M.Map({
                 container: 'map',
                 style: 'mapbox://styles/mapbox/streets-v11', // style URL
-                center: [-102.4, 37.7923], // starting position [lng, lat]
-                zoom: 3 // starting zoom
+                center: this.coords,
+                zoom: 7 // starting zoom
             })
 
             this.map.on('load', () => {
 
-
-
-                const amerob = ['87d318d15ffffff',
-                    '87d318d1dffffff',
-                    '87d318d33ffffff',
-                    '87d318c21ffffff',
-                    '87d318d1dffffff',
-                    '87d31eb49ffffff',
-                    '87d31eb6affffff',
-                    '87b0818d4ffffff',
-                    '87d318c2bffffff',
-                    '87d318c25ffffff',
-                    '87d31c69effffff',
-                    '87d31c6b2ffffff',
-                    '87d31eb68ffffff',
-                    '87d31c4caffffff',
-                    '87b0d2088ffffff',
-                    '87b0d2c01ffffff',
-                    '8792da590ffffff',
-                    '8792da4b4ffffff',
-                    '87d318c29ffffff',
-                    '87d318d56ffffff',
-                    '87d31eb6cffffff',
-                    '87d31c4c8ffffff',
-                    '87b0d20d4ffffff',
-                    '87b0d2c5effffff',
-                    '87b0d2d34ffffff',
-                    '87b0d64d9ffffff',
-                    '87d318d5affffff',
-                    '87d318d56ffffff',
-                    '87d318d24ffffff',
-                    '87d31c682ffffff',
-                    '87d31c6b0ffffff',
-                    '87d31c793ffffff',
-                    '87d31c4c8ffffff',
-                    '87d31c4e3ffffff',
-                    '87d318c71ffffff',
-                    '87d318d5effffff',
-                    '87d318d54ffffff',
-                    '87d31c680ffffff',
-                    '87d31c6b5ffffff',
-                    '87d31c791ffffff',
-                    '87d31c4cdffffff',
-                    '87d31c4e1ffffff',
-                    '87d31c41dffffff',
-                    '8792d3614ffffff',
-                    '87b0d21abffffff',
-                    '87b0d2d2effffff',
-                    '87b0d669dffffff',
-                    '87b081243ffffff',
-                    '87d318d5cffffff',
-                    '87d318d72ffffff']
-
-
-                const config = ({
-                    lng: -102.4,
-                    lat: 37.7923539,
-                    zoom: 11.5,
-                    fillOpacity: 0.6,
-                    colorScale: ['#ffffcc', '#78c679', '#006837']
+                const hexFromBbox = h3.polyfill(this.bboxRev, 4)
+                console.log(hexFromBbox)
+                const bbox4 = geojson2h3.h3SetToFeatureCollection(hexFromBbox)
+                this.map.addSource('test', {
+                    type: 'geojson',
+                    data: bbox4
                 })
 
-                // object formatted as {'h3codehere': dataval, 'h3codehere2': dataval, ...}
-                const hexCodes: HexObject = this.hexagons(config)
-                console.log(hexCodes)
+                this.map.addLayer({
+                    id: 'test',
+                    source: 'test',
+                    type: 'fill',
+                    maxzoom: 7,
+                    paint: {
+                        'fill-opacity': 0.2,
+                        'fill-color': 'blue'
+                    }
+                })
 
-                const geojson = geojson2h3.h3SetToFeatureCollection(Object.keys(hexCodes), hex => ({value: hexCodes[hex]}))
-                console.log(geojson)
 
-                // this.map.addSource('h3', {
+
+
+
+                //
+                // const parentIndex = h3.geoToH3(this.coords.lat, this.coords.lng, 4)
+                // // console.log(parentIndex)
+                //
+                // const parentGeoJson = geojson2h3.h3ToFeature(parentIndex)
+                // // console.log(parentGeoJson)
+                //
+                // this.map.addSource('test', {
                 //     type: 'geojson',
-                //     data: geojson
+                //     data: parentGeoJson
                 // })
                 //
                 // this.map.addLayer({
-                //     id: 'h3',
-                //     source: 'h3',
+                //     id: 'test',
+                //     source: 'test',
                 //     type: 'fill',
-                //     maxzoom: 4,
                 //     paint: {
-                //         'fill-color': 'deeppink',
-                //         'fill-opacity': 0.4
+                //         'fill-opacity': 0.2,
+                //         'fill-color': 'blue'
                 //     }
                 // })
-
-                const options: HexConfigOptions = {lat: 37.79, lng: -102.4, res: 3, rings: 10}
-                const geo = this.hexGeoJson(options)
-                this.addHexLayer('h3_1', geo, 'green', 4)
-
-                const options2: HexConfigOptions = {lat: 37.79, lng: -102.4, res: 4, rings: 30}
-                const geo2 = this.hexGeoJson(options2)
-                this.addHexLayer('h3_2', geo2, 'green', 6, 4)
-
-                const options3: HexConfigOptions = {lat: 37.79, lng: -102.4, res: 5, rings: 50}
-                const geo3 = this.hexGeoJson(options3)
-                this.addHexLayer('h3_3', geo3, 'green', 8, 6)
+                //
+                //
+                //
+                //
+                //
+                // const test = h3.geoToH3(this.coords.lat, this.coords.lng, 6)
+                // // console.log(parentIndex)
+                // const testg = geojson2h3.h3ToFeature(test)
+                // this.map.addSource('t', {
+                //     type: 'geojson',
+                //     data: testg
+                // })
+                //
+                // this.map.addLayer({
+                //     id: 't',
+                //     source: 't',
+                //     type: 'fill',
+                //     paint: {
+                //         'fill-opacity': 0.4,
+                //         'fill-color': 'red'
+                //     }
+                // })
+                //
+                //
+                //
+                //
+                // const children = h3.h3ToChildren(parentIndex, 5)
+                // // console.log(children)
+                // const childrenGeoJson = geojson2h3.h3SetToFeatureCollection(children)
+                //
+                // this.map.addSource('children', {
+                //     type: 'geojson',
+                //     data: childrenGeoJson
+                // })
+                //
+                // this.map.addLayer({
+                //     id: 'children',
+                //     source: 'children',
+                //     type: 'fill',
+                //     paint: {
+                //         'fill-opacity': 0.4,
+                //         'fill-color': 'yellow'
+                //     },
+                //     layout: {
+                //         visibility: 'none'
+                //     }
+                // })
+                //
+                // // const c = children.map(x => h3.h3ToChildren(x, 6))
+                //
+                // const c: string[] = []
+                // children.forEach(child => {
+                //     c.push(...h3.h3ToChildren(child, 6))
+                // })
+                //
+                // const cGeoJson = geojson2h3.h3SetToFeatureCollection(c)
+                //
+                // this.map.addSource('children2', {
+                //     type: 'geojson',
+                //     data: cGeoJson
+                // })
+                //
+                // this.map.addLayer({
+                //     id: 'children2',
+                //     source: 'children2',
+                //     type: 'fill',
+                //     paint: {
+                //         'fill-opacity': 0.4,
+                //         'fill-color': 'green'
+                //     }
+                // })
+                //
+                //
+                // //
+                // const config = ({
+                //     lng: -102.4,
+                //     lat: 37.7923539,
+                //     zoom: 11.5,
+                //     fillOpacity: 0.6,
+                //     colorScale: ['#ffffcc', '#78c679', '#006837']
+                // })
+                //
+                // // object formatted as {'h3codehere': dataval, 'h3codehere2': dataval, ...}
+                // const hexCodes: HexObject = this.hexagons(config)
+                // console.log(hexCodes)
+                //
+                // const geojson = geojson2h3.h3SetToFeatureCollection(Object.keys(hexCodes), hex => ({value: hexCodes[hex]}))
+                // console.log(geojson)
+                //
+                // // this.map.addSource('h3', {
+                // //     type: 'geojson',
+                // //     data: geojson
+                // // })
+                // //
+                // // this.map.addLayer({
+                // //     id: 'h3',
+                // //     source: 'h3',
+                // //     type: 'fill',
+                // //     maxzoom: 4,
+                // //     paint: {
+                // //         'fill-color': 'deeppink',
+                // //         'fill-opacity': 0.4
+                // //     }
+                // // })
+                //
+                // const options: HexConfigOptions = {lat: 37.79, lng: -102.4, res: 3, rings: 10}
+                // const geo = this.hexGeoJson(options)
+                // this.addHexLayer('h3_1', geo, 'green', 4)
+                //
+                // const options2: HexConfigOptions = {lat: 37.79, lng: -102.4, res: 4, rings: 30}
+                // const geo2 = this.hexGeoJson(options2)
+                // this.addHexLayer('h3_2', geo2, 'green', 6, 4)
+                //
+                // const options3: HexConfigOptions = {lat: 37.79, lng: -102.4, res: 5, rings: 50}
+                // const geo3 = this.hexGeoJson(options3)
+                // this.addHexLayer('h3_3', geo3, 'green', 8, 6)
 
 
             })
@@ -176,8 +243,8 @@
 
 <style lang="scss" scoped>
     #map {
-        width: 1000px;
-        height: 1000px;
+        width: 800px;
+        height: 800px;
         background: mediumvioletred;
     }
 </style>
