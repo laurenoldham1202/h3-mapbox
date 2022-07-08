00e464f7fefb1e540185010c4a359f868a820675
laurenoldham1202 <lauren.oldham@uky.edu>
2022-07-08 14:38:30 -0400

    Ad options to differentiate between drilling down and selecting, filter out only range selections

--- components/MapCleaned.vue
+++ components/MapCleaned.vue
@@ -1,5 +1,9 @@
 <template>
-	<div id="map-2"></div>
+	<span>
+		<div id="map-2"></div>
+		<button @click="selectMode = !selectMode">select mode: {{ selectMode }}</button>
+		<button @click="rangeOnly = !rangeOnly">show new range only {{ rangeOnly }}</button>
+	</span>
 </template>
 
 <script lang="ts">
@@ -57,12 +61,22 @@ export default Vue.extend({
 			[36.3116770845, -104.6527823561],
 		] as any,
 		selected: [] as any[],
+		selectMode: false,
+        rangeOnly: false,
+        ids: [] as any[]
 	}),
 	watch: {
 		selected(selected) {
 			// TODO Only push to array if not already in array?
 			console.log(Array.from(new Set(selected)))
 		},
+        rangeOnly() {
+		    if (this.rangeOnly) {
+		        console.log(this.ids)
+		        // this.map.getFeatureState()
+                this.map.setFilter('children', ['match', ['get', 'index'], Array.from(new Set(this.ids)), true, false])
+            }
+        }
 	},
 	mounted(): void {
 		;(M as any).accessToken = 'pk.eyJ1IjoibGF1cmVub2xkaGFtMTIwMiIsImEiOiJjaW55dm52N2gxODJrdWtseWZ5czAyZmp5In0.YkEUt6GvIDujjudu187eyA'
@@ -97,9 +111,9 @@ export default Vue.extend({
 				// tiles: ['http://localhost:8081/data/join-new-ids/{z}/{x}/{y}.pbf']
 			})
 
-            this.map.on('zoom', () => {
-                // console.log(this.map.getZoom())
-            })
+			this.map.on('zoom', () => {
+				// console.log(this.map.getZoom())
+			})
 
 			// this.map.addLayer({
 			// 	id: 'tiles',
@@ -112,23 +126,43 @@ export default Vue.extend({
 			// 	},
 			// })
 
+			// const shp = {"id":"e541903ac62aa1867de7f3cad8e9ae70","type":"Feature","properties":{},"geometry":{"coordinates":[[[-95.20742187499955,44.384407236660934],[-97.49257812499935,44.32156135607303],[-101.44765624999951,41.55552975462436],[-104.87539062499943,41.35791954134666],[-106.0179687499995,40.96089425396141],[-106.89687499999947,39.890427476779905],[-107.33632812499944,38.528478441103516],[-107.24843749999953,36.223971656062346],[-106.54531249999941,35.1532323637759],[-103.5570312499995,33.92247743491963],[-97.66835937499957,33.7764905296592],[-95.11953124999927,32.525594402240415],[-90.72499999999948,32.969122198383786],[-89.75820312499923,33.99537726897219],[-87.9124999999994,38.04557392217728],[-88.00039062499934,39.41675145008787],[-89.14296874999941,40.96089425396141],[-88.9671874999992,41.94894273855695],[-90.02187499999937,42.728526362280434],[-95.20742187499955,44.384407236660934],[-95.20742187499955,44.384407236660934]]],"type":"Polygon"}}
+			const shp = {
+				id: 'c55b82b34100d2c2d7eb2e060f42331b',
+				type: 'Feature',
+				properties: {},
+				geometry: {
+					coordinates: [
+						[
+							[-112.8926778080454, 46.496417563973324],
+							[-119.36422658381149, 46.55658712767425],
+							[-120.2387602021586, 45.03233987189347],
+							[-120.2387602021586, 42.828340710824364],
+							[-118.31478624179552, 42.44231676179456],
+							[-111.31851729502111, 42.37774669466478],
+							[-110.79379712401311, 43.719742554748535],
+							[-110.70634376217814, 46.31550893768946],
+							[-112.8926778080454, 46.496417563973324],
+							[-112.8926778080454, 46.496417563973324],
+						],
+					],
+					type: 'Polygon',
+				},
+			}
 
-            // const shp = {"id":"e541903ac62aa1867de7f3cad8e9ae70","type":"Feature","properties":{},"geometry":{"coordinates":[[[-95.20742187499955,44.384407236660934],[-97.49257812499935,44.32156135607303],[-101.44765624999951,41.55552975462436],[-104.87539062499943,41.35791954134666],[-106.0179687499995,40.96089425396141],[-106.89687499999947,39.890427476779905],[-107.33632812499944,38.528478441103516],[-107.24843749999953,36.223971656062346],[-106.54531249999941,35.1532323637759],[-103.5570312499995,33.92247743491963],[-97.66835937499957,33.7764905296592],[-95.11953124999927,32.525594402240415],[-90.72499999999948,32.969122198383786],[-89.75820312499923,33.99537726897219],[-87.9124999999994,38.04557392217728],[-88.00039062499934,39.41675145008787],[-89.14296874999941,40.96089425396141],[-88.9671874999992,41.94894273855695],[-90.02187499999937,42.728526362280434],[-95.20742187499955,44.384407236660934],[-95.20742187499955,44.384407236660934]]],"type":"Polygon"}}
-            const shp = {"id":"c55b82b34100d2c2d7eb2e060f42331b","type":"Feature","properties":{},"geometry":{"coordinates":[[[-112.8926778080454,46.496417563973324],[-119.36422658381149,46.55658712767425],[-120.2387602021586,45.03233987189347],[-120.2387602021586,42.828340710824364],[-118.31478624179552,42.44231676179456],[-111.31851729502111,42.37774669466478],[-110.79379712401311,43.719742554748535],[-110.70634376217814,46.31550893768946],[-112.8926778080454,46.496417563973324],[-112.8926778080454,46.496417563973324]]],"type":"Polygon"}}
-
-            this.map.addSource('shp', {
-                type: 'geojson',
-                data: shp
-            })
-            this.map.addLayer({
-                id: 'shp',
-                source: 'shp',
-                type: 'fill',
-                paint: {
-                    'fill-color': 'transparent',
-                    'fill-outline-color': 'purple'
-                }
-            })
+			this.map.addSource('shp', {
+				type: 'geojson',
+				data: shp,
+			})
+			this.map.addLayer({
+				id: 'shp',
+				source: 'shp',
+				type: 'fill',
+				paint: {
+					'fill-color': 'transparent',
+					'fill-outline-color': 'purple',
+				},
+			})
 			const hex = geojson2h3.featureToH3Set(USA, 3)
 			const geo = geojson2h3.h3SetToFeatureCollection(hex, (hex) => ({ index: hex }))
 
@@ -148,78 +182,92 @@ export default Vue.extend({
 				},
 			})
 
-            setTimeout(() => {
-            //
-            // console.log(shp.geometry, this.bboxToPixel(shp.geometry))
-                const int = this.map.queryRenderedFeatures(this.bboxToPixel(shp.geometry), { layers: ['usa'] })
-                const sel = int.map(x => x.id)
-                sel.forEach(hex => {
-
-                    this.map.setFeatureState({ source: 'usa', id: hex }, { selected: true })
-                })
-            }, 100)
-
-
-
-            this.map.addSource('children', {
-                type: 'geojson',
-                data: {},
-                promoteId: 'index'
-            })
-
-            const ch = []
-            this.map.addLayer({
-                id: 'children',
-                source: 'children',
-                type: 'fill',
-                paint: {
-                    'fill-opacity': 0.3,
-                    'fill-outline-color': 'blue',
-                    'fill-color': 'transparent'
-                },
-                layout: {
-                    'fill-sort-key': ['+', ['get', 'index']]
-                }
-            })
-
-
-            this.map.on('draw.create', (e) => {
-				const selected = this.map.queryRenderedFeatures(this.bboxToPixel(e.features[0].geometry), { layers: ['usa', 'children'] })
-				// console.log(selected)
-
-                console.log(JSON.stringify(e.features[0]))
-
-                // TODO Create different custom buttons for select and deselect?
-				selected.forEach((hex) => {
-				    // console.log(hex.id)
-				    // TODO filter out base layer and create new deeper hex layer?
-					// this.map.setFeatureState({ source: 'usa', id: hex.id }, { selected: true })
-
-                    //parseInt(hex.id[1]) + 1
-                    const res = parseInt(hex.id[1]) + 1
-                    const children = h3.h3ToChildren(hex.id, res > 8 ? 8 : res)
-                    // console.log(children)
-
-                    const geo2 = geojson2h3.h3SetToFeatureCollection(children, (hex) => ({ index: hex }))
-                    ch.push(...geo2.features)
-
-
-
+			setTimeout(() => {
+				//
+				// console.log(shp.geometry, this.bboxToPixel(shp.geometry))
+				const int = this.map.queryRenderedFeatures(this.bboxToPixel(shp.geometry), { layers: ['usa'] })
+				const sel = int.map((x) => x.id)
+				sel.forEach((hex) => {
+					// this.map.setFeatureState({ source: 'usa', id: hex }, { selected: true })
 				})
+			}, 100)
+
+			this.map.addSource('children', {
+				type: 'geojson',
+				data: {},
+				promoteId: 'index',
+			})
+
+			const ch = []
+			this.map.addLayer({
+				id: 'children',
+				source: 'children',
+				type: 'fill',
+				paint: {
+					'fill-opacity': 0.3,
+					'fill-outline-color': 'blue',
+					'fill-color': ['case', ['boolean', ['feature-state', 'selected'], false], 'deeppink', 'transparent'],
+				},
+				layout: {
+					'fill-sort-key': ['+', ['get', 'index']],
+				},
+			})
 
 
-                // console.log(ch)
-                this.map.getSource('children').setData({ type: 'FeatureCollection', features: Array.from(new Set(ch))})
-                this.map.setLayoutProperty('children', 'fill-sort-key', ['+', ['get', 'index']])
-				Draw.delete(e.features[0].id)
+			this.map.on('draw.create', (e) => {
+				if (!this.selectMode) {
+					const selected = this.map.queryRenderedFeatures(this.bboxToPixel(e.features[0].geometry), {
+						layers: ['usa', 'children'],
+					})
+					// console.log(selected)
 
-                setTimeout(() => {
-                    Draw.changeMode(Draw.modes.DRAW_POLYGON)
-                }, 10)
+					// console.log(JSON.stringify(e.features[0]))
 
-            })
+					// TODO Create different custom buttons for select and deselect?
+					selected.forEach((hex) => {
+						// console.log(hex.id)
+						// TODO filter out base layer and create new deeper hex layer?
+						// this.map.setFeatureState({ source: 'usa', id: hex.id }, { selected: true })
 
+						//parseInt(hex.id[1]) + 1
+						const res = parseInt(hex.id[1]) + 1
+						const children = h3.h3ToChildren(hex.id, res > 8 ? 8 : res)
+						// console.log(children)
 
+						const geo2 = geojson2h3.h3SetToFeatureCollection(children, (hex) => ({ index: hex }))
+						ch.push(...geo2.features)
+					})
+
+					// console.log(ch)
+					this.map.getSource('children').setData({
+						type: 'FeatureCollection',
+						features: Array.from(new Set(ch)),
+					})
+					this.map.setLayoutProperty('children', 'fill-sort-key', ['+', ['get', 'index']])
+					Draw.delete(e.features[0].id)
+
+					setTimeout(() => {
+						Draw.changeMode(Draw.modes.DRAW_POLYGON)
+					}, 10)
+				} else {
+				    // TODO Add button to increase resolution instead of redrawing concentric shapes
+					// console.log('select!')
+					const selected = this.map.queryRenderedFeatures(this.bboxToPixel(e.features[0].geometry), { layers: ['children'] })
+					const sorted = Array.from(new Set(selected.map((x) => x.id).sort()))
+					const res = sorted[sorted.length - 1][1]
+					// console.log(res)
+					selected.forEach((hex) => {
+						// console.log(hex.id)
+						// TODO filter out base layer and create new deeper hex layer?
+						// this.map.setFeatureState({ source: 'usa', id: hex.id }, { selected: true })
+                        if (hex.id[1] === res) {
+                            this.ids.push(hex.id)
+
+                            this.map.setFeatureState({ source: 'children', id: hex.id }, { selected: true })
+                        }
+					})
+				}
+			})
 
 			this.map.on('click', 'usa', (e: any) => {
 				// console.log(e.features[0])
@@ -228,12 +276,12 @@ export default Vue.extend({
 			this.map.on('click', 'children', (e: any) => {
 				// console.log(e.features[0])
 				const feature = e.features[0]
-                console.log(feature)
+				// console.log(feature)
 			})
 		})
 	},
 	methods: {
-	    // TODO Need to intersect and not use outright bbox
+		// TODO Need to intersect and not use outright bbox
 		bboxToPixel(polygon: any) {
 			// TODO type
 			// generate bounding box from polygon the user drew
@@ -288,4 +336,8 @@ export default Vue.extend({
 .mapboxgl-ctrl-group button {
 	color: black;
 }
+    button {
+        border: 1px solid black;
+        padding: 0.5rem
+    }
 </style>
