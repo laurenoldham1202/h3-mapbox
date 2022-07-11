91505632f6521cf9b5dfa308aa1871118fb9d300
laurenoldham1202 <lauren.oldham@uky.edu>
2022-07-11 15:28:54 -0400

    Test adjusting zl 7 and 8 manually

--- components/MapCleaned.vue
+++ components/MapCleaned.vue
@@ -3,6 +3,7 @@
 		<div id="map-2"></div>
 		<button @click="selectMode = !selectMode">select mode: {{ selectMode }}</button>
 		<button @click="rangeOnly = !rangeOnly">show new range only {{ rangeOnly }}</button>
+		<button @click="adjust">adjust</button>
 	</span>
 </template>
 
@@ -107,25 +108,28 @@ export default Vue.extend({
 			this.map.addSource('tiles', {
 				type: 'vector',
 				promoteId: 'h3_address',
-				tiles: ['http://localhost:8081/data/range-test/{z}/{x}/{y}.pbf'],
-				maxzoom: 9,
+				tiles: ['http://localhost:8081/data/woothr/{z}/{x}/{y}.pbf'],
+				maxzoom: 10,
 				// tiles: ['http://localhost:8081/data/join-new-ids/{z}/{x}/{y}.pbf']
 			})
 
 			this.map.on('zoom', () => {
-				// console.log(this.map.getZoom())
+				console.log(this.map.getZoom())
+                if (this.map.getZoom() >= 9) {
+
+                }
 			})
 
-			// this.map.addLayer({
-			// 	id: 'tiles',
-			// 	source: 'tiles',
-			// 	'source-layer': 'hex',
-			// 	type: 'fill',
-			// 	paint: {
-			// 		'fill-color': ['case', ['boolean', ['feature-state', 'selected'], false], 'deeppink', 'blue'],
-			// 		'fill-opacity': 0.3,
-			// 	},
-			// })
+			this.map.addLayer({
+				id: 'usa',
+				source: 'tiles',
+				'source-layer': 'hex',
+				type: 'fill',
+				paint: {
+					'fill-color': ['case', ['boolean', ['feature-state', 'selected'], false], 'deeppink', 'blue'],
+					'fill-opacity': 0.3,
+				},
+			})
 
 			// const shp = {"id":"e541903ac62aa1867de7f3cad8e9ae70","type":"Feature","properties":{},"geometry":{"coordinates":[[[-95.20742187499955,44.384407236660934],[-97.49257812499935,44.32156135607303],[-101.44765624999951,41.55552975462436],[-104.87539062499943,41.35791954134666],[-106.0179687499995,40.96089425396141],[-106.89687499999947,39.890427476779905],[-107.33632812499944,38.528478441103516],[-107.24843749999953,36.223971656062346],[-106.54531249999941,35.1532323637759],[-103.5570312499995,33.92247743491963],[-97.66835937499957,33.7764905296592],[-95.11953124999927,32.525594402240415],[-90.72499999999948,32.969122198383786],[-89.75820312499923,33.99537726897219],[-87.9124999999994,38.04557392217728],[-88.00039062499934,39.41675145008787],[-89.14296874999941,40.96089425396141],[-88.9671874999992,41.94894273855695],[-90.02187499999937,42.728526362280434],[-95.20742187499955,44.384407236660934],[-95.20742187499955,44.384407236660934]]],"type":"Polygon"}}
 			const shp = {
@@ -165,9 +169,10 @@ export default Vue.extend({
 			// 	},
 			// })
 
+            // FIXME this method doesn't allow deselection of original range boundaries
 			this.map.addSource('grpchi', {
 				type: 'vector',
-				tiles: ['http://localhost:8081/data/range-outline/{z}/{x}/{y}.pbf'],
+				tiles: ['http://localhost:8082/data/range-outline-2/{z}/{x}/{y}.pbf'],
                 maxzoom: 7,
 			})
 			this.map.addLayer({
@@ -177,12 +182,13 @@ export default Vue.extend({
 				type: 'fill',
                 // filter: ['>', ['get', 'resident'], 0],
 				paint: {
-					// 'fill-color': 'transparent',
-					// 'fill-outline-color': 'purple',
+					'fill-color': 'deeppink',
+                    'fill-opacity': 0.3,
+                    // 'fill-outline-color': 'purple',
 				},
 			})
 
-			const hex = geojson2h3.featureToH3Set(range, 3)
+			const hex = geojson2h3.featureToH3Set(range, 4)
 			const geo = geojson2h3.h3SetToFeatureCollection(hex, (hex) => ({ index: hex }))
 
             // const geojson = {"id":"8eb25ba70d452d97e1ddfcce5dd3c509","type":"Feature","properties":{},"geometry":{"coordinates":[[[-99.33506625318147,45.13314712628562],[-101.00487955708923,45.13314712628562],[-101.56148399172514,42.683079866666105],[-102.36546817508791,41.62863803346093],[-103.66421185590485,40.9782147291387],[-103.91159160463224,39.65795874143228],[-103.23129729563291,39.1322398894163],[-101.49963905454342,38.84381654664111],[-100.57196499681673,38.3119933246779],[-97.17049345181951,37.971513567475],[-97.04680357745609,36.84165053422949],[-96.6757339543653,36.49440699140514],[-94.6348510273673,36.792140282626605],[-93.58348709527718,38.36050350728348],[-95.68621495945743,38.55421925672027],[-95.50068014791177,39.61032990250396],[-96.98495864027439,40.791188855481806],[-96.73757889154703,41.44346493969201],[-96.18097445691163,41.90540315049239],[-96.49019914282017,43.000506465552206],[-96.79942382872926,43.3612818895958],[-98.28370232109134,43.496022931319146],[-98.84030675572728,44.29816727163572],[-98.77846181854555,45.00210923171318],[-99.08768650445464,45.08950120560945],[-99.33506625318147,45.13314712628562],[-99.33506625318147,45.13314712628562]]],"type":"Polygon"}}
@@ -191,21 +197,23 @@ export default Vue.extend({
 			// const g = geojson2h3.h3SetToFeature(h, (hex) => ({ index: hex }))
             // console.log(g)
 
-			this.map.addSource('usa', {
-				type: 'geojson',
-				data: geo,
-				promoteId: 'index',
-			})
 
-			this.map.addLayer({
-				id: 'usa',
-				source: 'usa',
-				type: 'fill',
-				paint: {
-					'fill-opacity': 0.3,
-					'fill-color': ['case', ['boolean', ['feature-state', 'selected'], false], 'deeppink', 'blue'],
-				},
-			})
+            //
+			// this.map.addSource('usa', {
+			// 	type: 'geojson',
+			// 	data: geo,
+			// 	promoteId: 'index',
+			// })
+            //
+			// this.map.addLayer({
+			// 	id: 'usa',
+			// 	source: 'usa',
+			// 	type: 'fill',
+			// 	paint: {
+			// 		'fill-opacity': 0.3,
+			// 		'fill-color': ['case', ['boolean', ['feature-state', 'selected'], false], 'deeppink', 'blue'],
+			// 	},
+			// })
 
 			setTimeout(() => {
 				//
@@ -237,6 +245,29 @@ export default Vue.extend({
 					'fill-sort-key': ['+', ['get', 'index']],
 				},
 			})
+
+
+
+
+
+
+            this.map.addSource('x', {
+                type: 'geojson',
+                data: {},
+                promoteId: 'index',
+            })
+
+            this.map.addLayer({
+                id: 'x',
+                source: 'x',
+                type: 'fill',
+                paint: {
+                    // 'fill-opacity': 0.3,
+                    // 'fill-color': ['case', ['boolean', ['feature-state', 'selected'], false], 'deeppink', 'blue'],
+                    'fill-color': 'transparent',
+                    'fill-outline-color': 'blue',
+                },
+            })
 
 
 			this.map.on('draw.create', (e) => {
@@ -314,6 +345,7 @@ export default Vue.extend({
                     console.log(feature)
                     // TODO Allow deselect
                     this.map.setFeatureState({ source: 'children', id: feature.id }, { selected: !feature.state.selected })
+                    this.ids.push(feature.id)
                 }
 
 			})
@@ -329,6 +361,18 @@ export default Vue.extend({
 		})
 	},
 	methods: {
+	    adjust() {
+	        const feats = this.map.queryRenderedFeatures({layers: ['usa']})
+            // TODO Add restriction on number of ids
+            const ids = feats.map(x => x.id)
+            const c = Array.from(new Set(this.getChildrenHexIndices(ids, 7)))
+            const geo = geojson2h3.h3SetToFeatureCollection(c, (hex) => ({ index: hex }))
+
+            console.log(geo)
+            // TODO This erases any previously plotted adjustments
+            this.map.getSource('x').setData(geo)
+
+        },
 		// TODO Need to intersect and not use outright bbox
 		bboxToPixel(polygon: any) {
 			// TODO type
