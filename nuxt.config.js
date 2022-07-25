e9a7bf2d3fa2666a95de5e9d59dcb6b96f9c3a1f
laurenoldham1202 <lauren.oldham@uky.edu>
2022-07-25 14:25:44 -0400

    Initialize github pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,10 @@
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
+  target: 'static',
+  router: {
+    base: '/h3-mapbox/'
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
