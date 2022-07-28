bac86225fe1e5cfc77ceb18eab835e2df88486bd
laurenoldham1202 <lauren.oldham@uky.edu>
2022-07-28 16:20:16 -0400

    Use deployed tiles where everything is one single layer

--- components/Map.vue
+++ components/Map.vue
@@ -3,7 +3,7 @@
 
     <!-- TODO Add button to reset hexes, add button to 'smooth' range -->
 <button @click="selectMode = !selectMode">Selection mode: {{ selectMode }}</button>
-<button @click="rangeOnly = !rangeOnly">show new range only: {{ rangeOnly }}</button>
+<!--<button @click="rangeOnly = !rangeOnly">show new range only: {{ rangeOnly }}</button>-->
 
 <div id="map-2"></div>
 
@@ -61,7 +61,7 @@
         container: 'map-2',
         style: 'mapbox://styles/mapbox/streets-v11', // style URL
         center: this.coords,
-        zoom: 6,
+        zoom: 2,
         doubleClickZoom: false,
       })
 
@@ -82,12 +82,7 @@
       this.map.addControl(new M.ScaleControl({maxWidth: 900}))
 
       this.map.on('load', () => {
-        this.map.addSource('base-hex', {
-          type: 'vector',
-          promoteId: 'h3_address',
-          tiles: ['http://localhost:8081/data/r3/{z}/{x}/{y}.pbf'],
-          maxzoom: 4,
-        })
+
 
         const rangeLine = {"id":"8eb25ba70d452d97e1ddfcce5dd3c509","type":"Feature","properties":{},"geometry":{"coordinates":[[[-99.33506625318147,45.13314712628562],[-101.00487955708923,45.13314712628562],[-101.56148399172514,42.683079866666105],[-102.36546817508791,41.62863803346093],[-103.66421185590485,40.9782147291387],[-103.91159160463224,39.65795874143228],[-103.23129729563291,39.1322398894163],[-101.49963905454342,38.84381654664111],[-100.57196499681673,38.3119933246779],[-97.17049345181951,37.971513567475],[-97.04680357745609,36.84165053422949],[-96.6757339543653,36.49440699140514],[-94.6348510273673,36.792140282626605],[-93.58348709527718,38.36050350728348],[-95.68621495945743,38.55421925672027],[-95.50068014791177,39.61032990250396],[-96.98495864027439,40.791188855481806],[-96.73757889154703,41.44346493969201],[-96.18097445691163,41.90540315049239],[-96.49019914282017,43.000506465552206],[-96.79942382872926,43.3612818895958],[-98.28370232109134,43.496022931319146],[-98.84030675572728,44.29816727163572],[-98.77846181854555,45.00210923171318],[-99.08768650445464,45.08950120560945],[-99.33506625318147,45.13314712628562],[-99.33506625318147,45.13314712628562]]],"type":"Polygon"}}
 
@@ -116,6 +111,14 @@
         // const compact = h3.compact(all)
         // console.log(compact)
 
+        this.map.addSource('base-hex', {
+          type: 'vector',
+          promoteId: 'h3_address',
+          // tiles: ['http://localhost:8083/data/all_clipped/{z}/{x}/{y}.pbf'],
+          tiles: ['https://test.cdn.shorebirdviz.ebird.org/range-map/test-2/{z}/{x}/{y}.pbf'],
+          maxzoom: 4,
+        })
+
         this.map.addLayer({
           id: 'base-hex',
           source: 'base-hex',
@@ -123,9 +126,12 @@
           type: 'fill',
           // filter: ['match', ['get', 'h3_address'], range2, false, true],
           paint: {
-            'fill-color': ['case', ['boolean', ['feature-state', 'selected'], false], 'deeppink', 'black'],
-            // 'fill-color': ['case', ['boolean', ['feature-state', 'selected'], ['match', ['get', 'h3_address'], range, true, false]], 'deeppink', 'black'],
+            // 'fill-color': ['case', ['boolean', ['feature-state', 'selected'], false], 'deeppink', 'black'],
+            'fill-color': ['case', ['boolean', ['feature-state', 'selected'], ['get', 'isRange']], 'deeppink', 'black'],
             'fill-opacity': 0.3,
+            // 'fill-opacity': 1,
+            // 'fill-outline-color': 'black',
+            // 'fill-color': 'yellow'
           },
         })
 
@@ -180,17 +186,19 @@
           maxzoom: 7,
           promoteId: 'h3_address'
         })
-        this.map.addLayer({
-          id: 'species-range',
-          source: 'species-range',
-          'source-layer': 'hex',
-          type: 'fill',
-          paint: {
-            'fill-color': ['case', ['boolean', ['feature-state', 'selected'], true], 'deeppink', 'transparent'],
-            'fill-opacity': 0.3,
-            'fill-outline-color': ['case', ['boolean', ['feature-state', 'selected'], true], 'deeppink', 'black'],
-          },
-        })
+
+
+        // this.map.addLayer({
+        //   id: 'species-range',
+        //   source: 'species-range',
+        //   'source-layer': 'hex',
+        //   type: 'fill',
+        //   paint: {
+        //     'fill-color': ['case', ['boolean', ['feature-state', 'selected'], true], 'deeppink', 'transparent'],
+        //     'fill-opacity': 0.3,
+        //     'fill-outline-color': ['case', ['boolean', ['feature-state', 'selected'], true], 'deeppink', 'black'],
+        //   },
+        // })
 
         // this.map.addSource('buffer', {
         //   type: 'vector',
